fa10d523c4f274830a27981c127ba956c6ae812c
Cyril Cohen <cohen@crans.org>
2018-04-17 18:18:08 +0200

    initial transfer

---
+++ theories/all_real_closed.v
@@ -0,0 +1,10 @@
+Require Export bigenough.
+Require Export cauchyreals.
+Require Export complex.
+Require Export ordered_qelim.
+Require Export polyorder.
+Require Export polyrcf.
+Require Export qe_rcf_th.
+Require Export qe_rcf.
+Require Export realalg.
+Require Export mxtens.
